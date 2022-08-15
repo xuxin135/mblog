4d26473489cc6c168a674e9df07b0ff70675f7e7
徐鑫 <bb2312@qq.com>
2022-08-15 22:51:03 +0800

    配置网关服务

--- mblog-web/src/main/java/com/xuxin/MblogWebApplication.java
+++ mblog-web/src/main/java/com/xuxin/MblogWebApplication.java
@@ -2,8 +2,10 @@ package com.xuxin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MblogWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(MblogWebApplication.class,args);
